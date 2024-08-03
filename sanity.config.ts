0a1dd7cda880489403282f4a796ba2c0d96777b9
javayhu <javayhu@gmail.com>
2024-08-03 15:51:04 +0800

    feat: config sanity studio & add plugins

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,66 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
-import { schemaTypes } from '@/sanity/schemas';
+import { codeInput } from '@sanity/code-input';
+import { colorInput } from '@sanity/color-input';
+import { dashboardTool, projectInfoWidget, projectUsersWidget, sanityTutorialsWidget } from "@sanity/dashboard";
+import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
+import { media } from 'sanity-plugin-media';
+import { schemaTypes } from '@/sanity/schemas/documents';
 
 export default defineConfig({
   name: 'default',
-  title: 'next-dir-app-studio',
+  title: 'NextDir',
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
 
   basePath: '/studio',
-  plugins: [structureTool(), visionTool()],
-
+  
   schema: {
     types: schemaTypes,
   },
+
+  plugins: [
+    // https://www.sanity.io/docs/structure-tool-api
+    // The Structure Tool is a top-level view within Sanity Studio 
+    // where editors can drill down to specific documents to edit them.
+    structureTool(), 
+    
+    // https://www.sanity.io/docs/the-vision-plugin
+    // Vision is a plugin that lets you quickly test your GROQ queries right from the Studio.
+    visionTool({
+      defaultApiVersion: '2024-08-01',
+      defaultDataset: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    }),
+
+    // https://www.sanity.io/docs/dashboard
+    // Dashboard is a Sanity Studio tool that allows you to add widgets that display information 
+    // about your content, project details, or anything else you'd want to put there.
+    dashboardTool({
+      widgets: [
+        projectInfoWidget(),
+        projectUsersWidget(
+          { layout: { width: 'large' } }
+        ),
+        sanityTutorialsWidget()
+      ],
+    }),
+
+    // https://www.sanity.io/plugins/sanity-plugin-media
+    // A convenient way to browse, manage and select all your Sanity assets.
+    media(),
+    
+    // https://www.sanity.io/plugins/color-input
+    // Color input for Sanity that stores selected colors in hex, hsl, hsv and rgb format.
+    colorInput(),
+    
+    // https://www.sanity.io/plugins/code-input
+    // Syntax highlighted editor for code.
+    codeInput(),
+    
+    // https://www.sanity.io/plugins/sanity-plugin-asset-source-unsplash
+    // Search photos on Unsplash and insert them directly inside of your Sanity Studio.
+    unsplashImageAsset(),
+  ],
 })

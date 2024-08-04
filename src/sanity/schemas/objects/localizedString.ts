7fae64494c1f4bcce7ce95471516e18cd024b532
javayhu <javayhu@gmail.com>
2024-08-04 13:02:19 +0800

    chore: add logs to localizedString

--- src/sanity/schemas/objects/localizedString.ts
+++ src/sanity/schemas/objects/localizedString.ts
@@ -2,10 +2,16 @@ import { defineField, defineType } from 'sanity';
 
 import { i18n } from '../../languages';
 
+/**
+ * https://www.sanity.io/docs/localization
+ */
 export default defineType({
   name: 'localizedString',
   title: 'Localized String',
   type: 'object',
+  // Fieldsets can be used to group object fields.
+  // Here we omit a fieldset for the "default language",
+  // making it stand out as the main field.
   fieldsets: [
     {
       title: 'Translations',
@@ -13,6 +19,7 @@ export default defineType({
       options: { collapsible: true, collapsed: false },
     },
   ],
+  // Dynamically define one field per language
   fields: i18n.languages.map((lang) =>
     defineField({
       name: lang.id,

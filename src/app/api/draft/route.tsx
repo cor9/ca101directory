a7c9fea811771f470843d32bb495f95e0e278110
javayhu <javayhu@gmail.com>
2024-08-04 08:01:57 +0800

    chore: optimize code

--- src/app/api/draft/route.tsx
+++ src/app/api/draft/route.tsx
@@ -1,7 +1,3 @@
-/**
- * This file is used to allow Presentation to set the app in Draft Mode, which will load Visual Editing
- * and query draft content and preview the content as it will appear once everything is published
- */
 import { validatePreviewUrl } from "@sanity/preview-url-secret";
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
@@ -9,9 +5,19 @@ import { redirect } from "next/navigation";
 import { sanityClient } from "@/sanity/lib/client";
 import { token } from "@/sanity/lib/token";
 
+/**
+ * 1.Draft Mode
+ * https://www.sanity.io/docs/draft-mode
+ * During runtime, switch between an optimized production mode and 
+ * an authorized mode to fetch draft content and render edit overlays.
+ * 
+ * 2.Presentation Tool
+ * https://www.sanity.io/docs/configuring-the-presentation-tool
+ * This file is used to allow Presentation to set the app in Draft Mode, which will load Visual Editing
+ * and query draft content and preview the content as it will appear once everything is published
+ */
 const clientWithToken = sanityClient.withConfig({ token });
 
-// This api route is used to allow PresentationTool in sanity studio to setup Draft Mode
 export async function GET(request: Request) {
   const { isValid, redirectTo = "/" } = await validatePreviewUrl(
     clientWithToken,

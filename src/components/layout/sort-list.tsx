9c42d5bb4d540d0014e8aa69bad2a6ec6b981179
javayhu <javayhu@gmail.com>
2024-08-11 12:22:06 +0800

    remove: remove filter-list & filter-item

--- src/components/layout/sort-list.tsx
+++
@@ -1,8 +0,0 @@
-import { sorting } from "@/lib/constants";
-import FilterList from "./filter/filter-list";
-
-export default function SortList() {
-    return (
-        <FilterList list={sorting} title="Sort by" />
-    );
-}

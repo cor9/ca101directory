15a94d41b666aa55c4730b504def4743d87de255
javayhu <javayhu@gmail.com>
2024-08-18 08:43:01 +0800

    feat: support immediate search after new input comes

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -3,43 +3,61 @@
 import { createUrl } from '@/lib/utils';
 import { SearchIcon } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { useDebounce } from 'use-debounce';
 
 export default function Search() {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState(searchParams?.get('q') || '');
+  const [debouncedQuery] = useDebounce(searchQuery, 300); // 300ms debounce
+  const lastExecutedQuery = useRef<string | null>(null);
 
-  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const val = e.target as HTMLFormElement;
-    const search = val.search as HTMLInputElement;
-    const newParams = new URLSearchParams(searchParams.toString());
-
-    if (search.value) {
-      newParams.set('q', search.value);
+  const updateSearchParams = useCallback((query: string) => {
+    const newParams = new URLSearchParams(searchParams?.toString() || '');
+    if (query) {
+      newParams.set('q', query);
     } else {
       newParams.delete('q');
     }
     newParams.delete('page');
-    router.push(createUrl('/search', newParams));
-  }
+    return newParams;
+  }, [searchParams]);
+
+  useEffect(() => {
+    const currentQuery = searchParams?.get('q');
+    console.log(`useEffect, currentQuery: ${currentQuery}, 
+      debouncedQuery: ${debouncedQuery}, 
+      lastExecutedQuery: ${lastExecutedQuery.current}`);
+    if (debouncedQuery !== currentQuery && debouncedQuery !== lastExecutedQuery.current) {
+      const newParams = updateSearchParams(debouncedQuery);
+      const newUrl = createUrl('/search', newParams);
+      console.log(`useEffect, newUrl: ${newUrl}`);
+      lastExecutedQuery.current = debouncedQuery;
+      router.push(newUrl, { scroll: false });
+    }
+  }, [debouncedQuery, router, updateSearchParams, searchParams]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
 
   return (
     <div className='flex items-center justify-center md:justify-start'>
-      <form onSubmit={onSubmit} className="w-80 relative">
+      <div className="w-80 relative">
         <input
-          key={searchParams?.get('q')}
           type="text"
           name="search"
           placeholder="Search..."
           autoComplete="off"
-          defaultValue={searchParams?.get('q') || ''}
+          value={searchQuery}
+          onChange={handleSearch}
           className="text-md w-full rounded-full border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
         />
         <div className="absolute right-0 top-0 mr-4 flex h-full items-center">
           <SearchIcon className="h-4" />
         </div>
-      </form>
+      </div>
     </div>
   );
 }

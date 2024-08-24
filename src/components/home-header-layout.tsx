8129205a5d7262e010ac029ba8b005833ab526f3
javayhu <javayhu@gmail.com>
2024-08-24 08:08:38 +0800

    feat: add home filter component to filter by caategory & tag & sort

--- src/components/home-header-layout.tsx
+++ src/components/home-header-layout.tsx
@@ -4,40 +4,59 @@ import HomeSearch from './home-search';
 import { SearchSkeleton } from './search';
 import MaxWidthWrapper from './shared/max-width-wrapper';
 import { SortList } from './sort-list';
+import { CategoryListQueryResult, TagListQueryResult } from '@/sanity.types';
+import { sanityFetch } from '@/sanity/lib/fetch';
+import { categoryListQuery, tagListQuery } from '@/sanity/lib/queries';
+import { HomeFilter } from './home-filter';
 
 export async function HomeHeaderLayout() {
+  const categoryList = await sanityFetch<CategoryListQueryResult>({
+    query: categoryListQuery
+  });
+  const tagList = await sanityFetch<TagListQueryResult>({
+    query: tagListQuery
+  });
+
+  // convert categoryList to array of objects with id and name
+  const categories = categoryList.map((category) => ({
+    slug: category.slug.current,
+    name: category.name.find((name) => name._key === 'en')?.value || '',
+  }));
+  const tags = tagList.map((tag) => ({
+    slug: tag.slug.current,
+    name: tag.name.find((name) => name._key === 'en')?.value || '',
+  }));
+
   return (
     <>
       {/* Desktop View, has MaxWidthWrapper */}
-      <MaxWidthWrapper className="hidden md:block">
-        <div className='grid grid-cols-[1fr_150px] gap-8 items-center justify-between'>
-          <div className="w-full min-w-0 mt-4">
-            <Suspense fallback={<SearchSkeleton />}>
-              <HomeSearch />
-            </Suspense>
-          </div>
-
-          {/* set width to 150px */}
-          <div className="mt-4">
-          <Suspense fallback={null}>
-            <SortList sortList={sorting} />
-          </Suspense>
-          </div>
-        </div>
-      </MaxWidthWrapper>
-
-      {/* Mobile View, no MaxWidthWrapper */}
-      <div className="md:hidden flex flex-col gap-8">
-        <div className='w-full min-w-0 mt-4'>
+      <MaxWidthWrapper className="hidden md:flex md:flex-col">
+      <div className='w-full'>
           <Suspense fallback={<SearchSkeleton />}>
             <HomeSearch />
           </Suspense>
         </div>
 
-        {/* set width to full */}
-        <div className="w-full mb-8">
-          <Suspense fallback={null}>
-            <SortList sortList={sorting} />
+        <div className='flex items-center justify-between'>
+          <div className="w-full">
+            <Suspense fallback={<SearchSkeleton />}>
+              <HomeFilter tagList={tags} categoryList={categories} sortList={sorting} />
+            </Suspense>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+
+      {/* Mobile View, no MaxWidthWrapper */}
+      <div className="md:hidden flex flex-col">
+        <div className='w-full'>
+          <Suspense fallback={<SearchSkeleton />}>
+            <HomeSearch />
+          </Suspense>
+        </div>
+
+        <div className="mx-4">
+          <Suspense fallback={<SearchSkeleton />}>
+            <HomeFilter tagList={tags} categoryList={categories} sortList={sorting} />
           </Suspense>
         </div>
       </div>

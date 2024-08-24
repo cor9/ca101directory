5898638e0a5f427115a755635c965a824014ea4a
javayhu <javayhu@gmail.com>
2024-08-24 10:29:18 +0800

    chore: show no results in item-grid

--- src/components/item-grid.tsx
+++ src/components/item-grid.tsx
@@ -10,18 +10,29 @@ interface ItemGridProps {
 
 export default function ItemGrid({ items, totalPages }: ItemGridProps) {
   return (
-    <section className=''>
-      <div className="my-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {items.length > 0 && items.map((item) => (
-          <ItemCard key={item._id} item={item} />
-        ))}
-      </div>
+    <>
+      {
+        items && items.length > 0 ? (
+          <section className=''>
+            <div className="my-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {items.length > 0 && items.map((item) => (
+                <ItemCard key={item._id} item={item} />
+              ))}
+            </div>
 
-      <div className="my-8 w-full flex items-center justify-center">
-        <Suspense fallback={null}>
-          <CustomPagination totalPages={totalPages} />
-        </Suspense>
-      </div>
-    </section>
+            <div className="my-8 w-full flex items-center justify-center">
+              <Suspense fallback={null}>
+                <CustomPagination totalPages={totalPages} />
+              </Suspense>
+            </div>
+          </section>
+        ) : (
+          // border rounded-lg bg-accent/25 
+          <div className="my-8 h-24 w-full flex items-center justify-center">
+            <p>No Results Found</p>
+          </div>
+        )
+      }
+    </>
   );
 }

27a279cc58d1b3adfa327f37a27b9d548eac5c82
javayhu <hujiawei090807@gmail.com>
2024-09-18 07:19:47 +0800

    fix: fix build error

--- src/app/(website)/(protected)/layout-02.tsx
+++ src/app/(website)/(protected)/layout-02.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/navbar";
 import Container from "@/components/shared/container";
+import { marketingConfig } from "@/config/marketing";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -9,7 +10,7 @@ interface MarketingLayoutProps {
 export default async function MarketingLayout({ children }: MarketingLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar scroll={true} />
+      <Navbar scroll={true} config={marketingConfig} />
 
       <main className="flex-1">
         <Container className="pb-16">

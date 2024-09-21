0e19592f09247e0b9984cf274facfb6ec2cf2ad3
javayhu <hujiawei090807@gmail.com>
2024-09-21 17:07:44 +0800

    chore: optimize sanity client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,7 +2,6 @@ import { createClient } from 'next-sanity';
 import { SanityClient } from 'sanity';
 import { apiVersion, dataset, projectId } from './api';
 import { token } from './token';
-import { postquery, singlequery, pathquery, postsbyauthorquery, catpathquery, postsbycatquery, catquery, paginatedquery } from './queries';
 
 export const sanityClient: SanityClient = createClient({
   projectId,
@@ -10,99 +9,7 @@ export const sanityClient: SanityClient = createClient({
   apiVersion,
   perspective: "published",
   useCdn: process.env.NODE_ENV === 'production',
-  token, // when write data to sanity, token with write permission is required!
-  // proxy: 'socks5://127.0.0.1:7890',
-  // maxRetries: 1,
-  // timeout: 10000,
+
+  // TODO:when write data to sanity, token with write permission is required!
+  token,
 });
-
-
-// Blog queries
-// import { postquery, singlequery, pathquery, authorsquery, postsbyauthorquery, allauthorsquery, catpathquery, postsbycatquery, catquery, paginatedquery } from './queries';
-export async function getAllPosts() {
-  if (sanityClient) {
-    return (await sanityClient.fetch(postquery)) || [];
-  }
-  return [];
-}
-
-// export async function getSettings() {
-//   if (sanityClient) {
-//     return (await sanityClient.fetch(configQuery)) || [];
-//   }
-//   return [];
-// }
-
-export async function getPostBySlug(slug) {
-  if (sanityClient) {
-    return (await sanityClient.fetch(singlequery, { slug })) || {};
-  }
-  return {};
-}
-
-export async function getAllPostsSlugs() {
-  if (sanityClient) {
-    const slugs = (await sanityClient.fetch(pathquery)) || [];
-    return slugs.map(slug => ({ slug }));
-  }
-  return [];
-}
-// Author
-// export async function getAllAuthorsSlugs() {
-//   if (sanityClient) {
-//     const slugs = (await sanityClient.fetch(authorsquery)) || [];
-//     return slugs.map(slug => ({ author: slug }));
-//   }
-//   return [];
-// }
-
-export async function getAuthorPostsBySlug(slug) {
-  if (sanityClient) {
-    return (await sanityClient.fetch(postsbyauthorquery, { slug })) || {};
-  }
-  return {};
-}
-
-// export async function getAllAuthors() {
-//   if (sanityClient) {
-//     return (await sanityClient.fetch(allauthorsquery)) || [];
-//   }
-//   return [];
-// }
-
-// Category
-
-export async function getAllCategories() {
-  if (sanityClient) {
-    const slugs = (await sanityClient.fetch(catpathquery)) || [];
-    return slugs.map(slug => ({ category: slug }));
-  }
-  return [];
-}
-
-export async function getPostsByCategory(slug) {
-  if (sanityClient) {
-    return (await sanityClient.fetch(postsbycatquery, { slug })) || {};
-  }
-  return {};
-}
-
-export async function getTopCategories() {
-  if (sanityClient) {
-    return (await sanityClient.fetch(catquery)) || [];
-  }
-  return [];
-}
-
-export async function getPaginatedPosts({ limit, pageIndex = 0 }) {
-  console.log(paginatedquery);
-  if (sanityClient) {
-    return (
-      (await sanityClient.fetch(paginatedquery, {
-        pageIndex: pageIndex,
-        limit: limit
-      })) || []
-    );
-  }
-  return [];
-}

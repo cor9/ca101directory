b9206438e60ddd3e093a46181afe2165275116c5
javayhu <hujiawei090807@gmail.com>
2024-10-02 07:29:20 +0800

    chore: reset authjs route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,25 @@
 import { NextRequest } from 'next/server';
 import { handlers } from '@/auth';
 
+export const { GET, POST } = handlers;
+
 /**
  * in case you want to debug the auth process
  */
-const handler = async (request: NextRequest) => {
-    console.log('Handling auth request:', request.url);
-    // console.log('Request cookies:', request.headers.get('cookie'));
+// const handler = async (request: NextRequest) => {
+//     console.log('Handling auth request:', request.url);
+//     // console.log('Request cookies:', request.headers.get('cookie'));
 
-    const result = await handlers.GET(request);
+//     const result = await handlers.GET(request);
 
-    // console.log('Auth response:', result);
-    // const setCookieHeader = result.headers?.get('Set-Cookie');
-    // if (setCookieHeader) {
-    //     console.log('Set-Cookie header:', setCookieHeader);
-    // } else {
-    //     console.log('No Set-Cookie header found');
-    // }
+//     // console.log('Auth response:', result);
+//     // const setCookieHeader = result.headers?.get('Set-Cookie');
+//     // if (setCookieHeader) {
+//     //     console.log('Set-Cookie header:', setCookieHeader);
+//     // } else {
+//     //     console.log('No Set-Cookie header found');
+//     // }
 
-    return result;
-};
-
-export { handler as GET, handler as POST };
+//     return result;
+// };
+// export { handler as GET, handler as POST };

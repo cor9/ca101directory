26c99eb80688ffe51e0e8684b5518ac4dd9b0c73
javayhu <hujiawei090807@gmail.com>
2024-10-09 23:39:25 +0800

    feat: add posthog analytics

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -6,14 +6,15 @@ import {
   fontSourceSerif,
   fontWorkSans,
 } from "@/assets/fonts";
+import { auth } from "@/auth";
+import { Analytics } from "@/components/analytics/analytics";
+import { CSPostHogProvider } from "@/components/analytics/posthog-analytics";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Toaster } from "@/components/ui/sonner";
+import { constructMetadata } from "@/lib/metadata";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { auth } from "@/auth";
-import { constructMetadata } from "@/lib/metadata";
-import { Analytics } from "@/components/analytics/analytics";
 
 export const metadata = constructMetadata();
 
@@ -24,41 +25,43 @@ interface RootLayoutProps {
 export default async function RootLayout({ children }: RootLayoutProps) {
   // https://youtu.be/1MTyCvS05V4?t=21464
   const session = await auth();
-  
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background antialiased",
-          fontBricolage.className,
-          // fontSourceSans.className,
-          // fontSourceSerif.className,
-          // fontWorkSans.className,
-          fontSourceSerif.variable,
-          fontSourceSans.variable,
-          fontWorkSans.variable,
-          fontBricolage.variable,
-        )}
-      >
-        <SessionProvider session={session}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange>
+      <CSPostHogProvider>
+        <body
+          className={cn(
+            "min-h-screen bg-background antialiased",
+            fontBricolage.className,
+            // fontSourceSans.className,
+            // fontSourceSerif.className,
+            // fontWorkSans.className,
+            fontSourceSerif.variable,
+            fontSourceSans.variable,
+            fontWorkSans.variable,
+            fontBricolage.variable,
+          )}
+        >
+          <SessionProvider session={session}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange>
 
-            {children}
+              {children}
 
-            {/* https://sonner.emilkowal.ski/toaster */}
-            <Toaster richColors position="top-right" offset={64} />
+              {/* https://sonner.emilkowal.ski/toaster */}
+              <Toaster richColors position="top-right" offset={64} />
 
-            <TailwindIndicator />
+              <TailwindIndicator />
 
-            <Analytics />
-          </ThemeProvider>
-        </SessionProvider>
-      </body>
+              <Analytics />
+            </ThemeProvider>
+          </SessionProvider>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }

0fc64d63b06b1078fb4f18e0d7227eb0e24a7896
javayhu <javayhu@tencent.com>
2024-10-09 09:54:26 +0800

    fix: fix sitemap url in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,15 @@
+import { siteConfig } from "@/config/site";
 import { MetadataRoute } from "next";
 
+/**
+ * https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+ */
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "/sitemap.xml",
+    sitemap: `${siteConfig.url}/sitemap.xml`,
   }
 }

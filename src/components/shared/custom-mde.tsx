b4e9dcd1380788a4d7a1c958e3222b7a31806ea5
javayhu <hujiawei090807@gmail.com>
2024-10-13 07:23:47 +0800

    fix: fix Function components cannot be given refs. Attempts to access this ref will fail, Did you mean to use React.forwardRef? Check the render method of 'SlotClone'

--- src/components/shared/custom-mde.tsx
+++ src/components/shared/custom-mde.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { useMemo } from "react";
+import React from "react";
 
 // if directly import, frontend error: document is not defined
 // import { SimpleMdeReact } from "react-simplemde-editor";
@@ -30,55 +31,59 @@ interface CustomMdeProps {
  *
  * https://github.com/sanity-io/sanity-plugin-markdown?tab=readme-ov-file#customizing-editor-preview
  */
-const CustomMde: React.FC<CustomMdeProps> = ({ value, onChange }) => {
-  const theme = useTheme();
+const CustomMde = React.forwardRef<HTMLDivElement, CustomMdeProps>(
+  ({ value, onChange }, ref) => {
+    const theme = useTheme();
 
-  // https://github.com/RIP21/react-simplemde-editor?tab=readme-ov-file#options
-  // useMemo to memoize options so they do not change on each rerender
-  // https://github.com/Ionaru/easy-markdown-editor?tab=readme-ov-file#options-example
-  // don't show image or upload-image button, images are uploaded in the image field of form
-  // don't show side-by-side, it will trigger fullscreen and has some bugs in UI
-  // show preview? tailwindcss reset all styles, so by default the preview is not working,
-  // but if add class `prose` to the previewClass, it will work, but not perfect
-  const mdeOptions = useMemo(() => {
-    return {
-      status: false,
-      autofocus: false,
-      spellChecker: false,
-      placeholder: "Enter your content here...",
-      toolbar: [
-        "heading",
-        "bold",
-        "italic",
-        "strikethrough",
-        "code",
-        "quote",
-        "unordered-list",
-        "ordered-list",
-        "link",
-        "preview",
-        "guide",
-      ],
-      previewClass: [
-        "prose",
-        "prose-slate",
-        "dark:prose-invert",
-        "bg-background",
-        "text-foreground",
-      ],
-    } as SimpleMDE.Options;
-  }, []);
+    // https://github.com/RIP21/react-simplemde-editor?tab=readme-ov-file#options
+    // useMemo to memoize options so they do not change on each rerender
+    // https://github.com/Ionaru/easy-markdown-editor?tab=readme-ov-file#options-example
+    // don't show image or upload-image button, images are uploaded in the image field of form
+    // don't show side-by-side, it will trigger fullscreen and has some bugs in UI
+    // show preview? tailwindcss reset all styles, so by default the preview is not working,
+    // but if add class `prose` to the previewClass, it will work, but not perfect
+    const mdeOptions = useMemo(() => {
+      return {
+        status: false,
+        autofocus: false,
+        spellChecker: false,
+        placeholder: "Enter your content here...",
+        toolbar: [
+          "heading",
+          "bold",
+          "italic",
+          "strikethrough",
+          "code",
+          "quote",
+          "unordered-list",
+          "ordered-list",
+          "link",
+          "preview",
+          "guide",
+        ],
+        previewClass: [
+          "prose",
+          "prose-slate",
+          "dark:prose-invert",
+          "bg-background",
+          "text-foreground",
+        ],
+      } as SimpleMDE.Options;
+    }, []);
 
-  return (
-    <div data-theme={theme} style={{ maxWidth: "none" }}>
-      <SimpleMdeReact
-        options={mdeOptions}
-        value={value}
-        onChange={onChange}
-        className="shadow-none"
-      />
-    </div>
-  );
-};
+    return (
+      <div ref={ref} data-theme={theme} style={{ maxWidth: "none" }}>
+        <SimpleMdeReact
+          options={mdeOptions}
+          value={value}
+          onChange={onChange}
+          className="shadow-none"
+        />
+      </div>
+    );
+  }
+);
+
+CustomMde.displayName = "CustomMde";
 
 export default CustomMde;

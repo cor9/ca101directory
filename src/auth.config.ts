906b600a1e5109b9de53ce63760694492e393b4b
javayhu <javayhu@tencent.com>
2024-10-15 09:20:27 +0800

    fix: remove trustHost in auth config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -13,11 +13,12 @@ import { verifyPassword } from "./lib/password";
  */
 export default {
   // https://authjs.dev/getting-started/migrating-to-v5#environment-variables
+  // https://authjs.dev/getting-started/deployment#auth_trust_host
   // The AUTH_TRUST_HOST environment variable serves the same purpose as setting trustHost: true in your Auth.js configuration.
   // This is necessary when running Auth.js behind a proxy.
   // When set to true we will trust the X-Forwarded-Host and X-Forwarded-Proto headers
   // passed to the app by the proxy to auto-detect the host URL (AUTH_URL)
-  trustHost: true,
+  // trustHost: true,
   providers: [
     // https://authjs.dev/getting-started/authentication/oauth
     GitHub,

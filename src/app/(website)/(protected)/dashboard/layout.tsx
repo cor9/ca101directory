08556e6abf324d99a358fde89d8a0388c07d4d4c
javayhu <hujiawei090807@gmail.com>
2024-11-02 23:05:13 +0800

    chore: support navigate to studio from dashboard page

--- src/app/(website)/(protected)/dashboard/layout.tsx
+++ src/app/(website)/(protected)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { DashboardButton } from "@/components/dashboard/dashboard-button";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { Button } from "@/components/ui/button";
 import { UploadIcon } from "lucide-react";
@@ -9,16 +10,20 @@ export default async function DashboardLayout({
   return (
     <div>
       <DashboardHeader title="Dashboard" subtitle="Overview of submissions">
-        <Button asChild disabled className="group whitespace-nowrap">
-          <Link
-            href="/submit"
-            prefetch={false}
-            className="flex items-center justify-center space-x-2"
-          >
-            <UploadIcon className="w-4 h-4" />
-            <span>Submit</span>
-          </Link>
-        </Button>
+        <div className="flex items-center space-x-4">
+          <DashboardButton />
+
+          <Button asChild disabled className="group whitespace-nowrap">
+            <Link
+              href="/submit"
+              prefetch={false}
+              className="flex items-center justify-center space-x-2"
+            >
+              <UploadIcon className="w-4 h-4" />
+              <span>Submit</span>
+            </Link>
+          </Button>
+        </div>
       </DashboardHeader>
 
       <div className="mt-8">{children}</div>

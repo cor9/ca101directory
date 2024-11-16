302ac53d79d8a2b6bacf1c6c792ce7a2965e0ea0
javayhu <hujiawei090807@gmail.com>
2024-11-16 14:57:34 +0800

    chore: optimize hero config

--- src/config/hero.ts
+++ src/config/hero.ts
@@ -2,14 +2,14 @@ import type { HeroConfig } from "@/types";
 
 export const heroConfig: HeroConfig = {
   title: {
-    first: "The Ultimate",
+    first: "The Best",
     second: "Directory Website Template",
   },
   subtitle:
-    "This is a demo directory website built with Mkdirs, with which you can launch any trending and profitable directory website in minutes without hassle.",
+    "This is a demo directory website built with Mkdirs",
   label: {
     text: "Introducing Mkdirs on",
-    href: "https://x.com/MkdirsHQ",
+    href: "https://x.com/javayhu",
     icon: "twitter",
   },
 };

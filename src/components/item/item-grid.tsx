e75aebd827d7154a57ecfac3791bdada3bf46131
javayhu <hujiawei090807@gmail.com>
2024-12-08 20:08:04 +0800

    chore: optimize item card and grid

--- src/components/item/item-grid.tsx
+++ src/components/item/item-grid.tsx
@@ -1,4 +1,4 @@
-import ItemCard from "@/components/item/item-card";
+import ItemCard, { ItemCardSkeleton } from "@/components/item/item-card";
 import { ITEMS_PER_PAGE, SUPPORT_ITEM_ICON } from "@/lib/constants";
 import type { ItemListQueryResult } from "@/sanity.types";
 import { Skeleton } from "../ui/skeleton";
@@ -33,11 +33,7 @@ export function ItemGridSkeleton({
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {[...Array(count)].map((_, index) => (
         // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-        <div key={index} className="flex flex-col gap-2">
-          <Skeleton className="w-full aspect-[16/9]" />
-          <Skeleton className="h-8 w-1/2" />
-          <Skeleton className="h-12 w-full" />
-        </div>
+        <ItemCardSkeleton key={index} />
       ))}
     </div>
   );

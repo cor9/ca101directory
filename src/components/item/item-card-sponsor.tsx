93cc985141d35580d79faebbc3ddc3706c98cb71
javayhu <hujiawei090807@gmail.com>
2024-12-18 21:33:34 +0800

    chore: optimize sponsor item

--- src/components/item/item-card-sponsor.tsx
+++ src/components/item/item-card-sponsor.tsx
@@ -22,7 +22,9 @@ export default function SponsorItemCard({ item }: SponsorItemCardProps) {
   const itemLink = getItemTargetLinkInWebsite(item);
 
   return (
-    <div
+    <Link
+      href={itemLink}
+      target="_blank"
       className={cn(
         "border rounded-lg flex flex-col justify-between p-6",
         "duration-300 shadow-sm hover:shadow-md transition-shadow",
@@ -50,10 +52,7 @@ export default function SponsorItemCard({ item }: SponsorItemCardProps) {
             />
           )}
 
-          <Link
-            href={`${itemLink}`}
-            className="flex-1 flex justify-between items-center"
-          >
+          <div className="flex-1 flex justify-between items-center">
             <h3
               className={cn(
                 "flex-1 text-xl font-medium line-clamp-1 flex items-center gap-2",
@@ -66,81 +65,32 @@ export default function SponsorItemCard({ item }: SponsorItemCardProps) {
             <span className="text-sm text-sky-500 border border-sky-500 rounded-md px-2 py-0.5">
               AD
             </span>
-          </Link>
+          </div>
         </div>
 
-        {/* categories */}
-        {/* <div className="flex flex-col gap-2">
-          {item.categories && item.categories.length > 0 && (
-            <div className="flex flex-wrap gap-2 items-center">
-              {item.categories.map((category, index) => (
-                <a
-                  key={category._id}
-                  href={`/category/${category.slug.current}`}
-                  className={cn(
-                    buttonVariants({ variant: "outline", size: "sm" }),
-                    "px-2 py-1 h-6 rounded-md",
-                  )}
-                >
-                  <span className="text-sm text-muted-foreground">
-                    {category.name}
-                  </span>
-                </a>
-              ))}
-            </div>
-          )}
-        </div> */}
-
-        {/* min-h-[4.5rem] is used for making sure height of the card is the same */}
-        <Link href={`${itemLink}`} className="mt-4 block cursor-pointer">
-          <p className="text-sm line-clamp-3 leading-relaxed min-h-[4.5rem]">
-            {item.description}
-          </p>
-        </Link>
+        {/* description */}
+        <p className="text-sm line-clamp-3 leading-relaxed min-h-[4.5rem]">
+          {item.description}
+        </p>
       </div>
 
       {/* bottom */}
       <div className="mt-4 w-full flex justify-center items-center">
-        {/* {item.tags && item.tags.length > 0 && (
-          <div className="flex flex-wrap gap-2 items-center">
-            {item.tags.slice(0, 5).map((tag, index) => (
-              <Link
-                key={tag._id}
-                href={`/tag/${tag.slug.current}`}
-                className="flex items-center justify-center space-x-0.5 group"
-              >
-                <HashIcon className="w-3 h-3 text-muted-foreground icon-scale" />
-                <span className="text-sm text-muted-foreground link-underline">
-                  {tag.name}
-                </span>
-              </Link>
-            ))}
-            {item.tags.length > 5 && (
-              <span className="text-sm text-muted-foreground px-1">
-                +{item.tags.length - 5}
-              </span>
-            )}
-          </div>
-        )} */}
-
         {/* action button */}
         <Button
           size="lg"
           variant="outline"
           className={cn(
-            "overflow-hidden rounded-full w-full border-sky-300 bg-transparent hover:bg-transparent",
+            "overflow-hidden rounded-full w-full bg-sky-500 hover:bg-sky-600",
             "group transition-transform duration-300 ease-in-out hover:scale-105",
           )}
         >
-          <Link
-            href={itemLink}
-            className="flex items-center justify-center gap-2"
-          >
-            <span className="text-sky-500 font-semibold">Visit Website</span>
-            <ArrowUpRightIcon className="text-sky-500 font-semibold size-4 transition-transform group-hover:translate-x-1" />
-          </Link>
+          <div className="flex items-center justify-center gap-2">
+            <span className="text-white font-semibold">Visit Website</span>
+            <ArrowUpRightIcon className="text-white font-semibold size-4 transition-transform group-hover:translate-x-1" />
+          </div>
         </Button>
       </div>
-    </div>
+    </Link>
   );
 }

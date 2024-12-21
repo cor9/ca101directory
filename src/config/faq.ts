7906cb258b0f63ad32c709c59bc1e95336e43c3f
javayhu <hujiawei090807@gmail.com>
2024-12-21 11:15:12 +0800

    chore: update faq & optimize link style

--- src/config/faq.ts
+++ src/config/faq.ts
@@ -8,21 +8,21 @@ export const faqConfig: FAQConfig = {
     question: "Is it free to submit my product?",
     answer:
       "Yes, it is. \nYou can submit your product for free to get 3 dofollow links to boost your SEO. \nHowever, the free plan has limitations:\n" +
-      "• Reviewed and listed within 72 hours\n" +
-      "• Requires a backlink to our site\n" +
-      "• No customer support",
+      "- Reviewed and listed within 72 hours\n" +
+      "- Requires a backlink to our site\n" +
+      "- No customer support",
   },
   {
     id: "item-2",
     question: "What are the benefits of the Pro plan?",
     answer:
       "The Pro plan offers several benefits:\n" +
-      "• At least 3 dofollow links\n" +
-      "• Immediate listing, or publish it whenever you want\n" +
-      "• Permanent links with no backlink requirement\n" +
-      "• Featured in listings with an award icon\n" +
-      "• Promotion through our social media and newsletters\n" +
-      "• Premium customer support",
+      "- At least 3 dofollow links\n" +
+      "- Immediate listing, or publish it whenever you want\n" +
+      "- Permanent links with no backlink requirement\n" +
+      "- Featured in listings with an award icon\n" +
+      "- Promotion through our social media and newsletters\n" +
+      "- Premium customer support",
   },
   {
     id: "item-3",
@@ -35,7 +35,7 @@ export const faqConfig: FAQConfig = {
     id: "item-4",
     question: "Do I need to provide a backlink for my listing?",
     answer:
-      `For the Free plan, a backlink to our site is required. \nThe backlink is <a href='${siteConfig.url}' title='${siteConfig.name}'>${siteConfig.name}</a>. \nHowever, if you choose the Pro plan, you get a permanent link without any backlink requirement.`,
+      `For the Free plan, a backlink to our site is required. \nThe backlink is <a href='${siteConfig.url}' class='underline text-primary' title='${siteConfig.name}'>${siteConfig.url}</a>. \nHowever, if you choose the Pro plan, you get a permanent link without any backlink requirement.`,
     },
   ],
 };

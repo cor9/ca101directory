1580495c76465977559b7b850205eb2dd533212e
javayhu <hujiawei090807@gmail.com>
2024-12-21 11:15:12 +0800

    chore: update faq & optimize link style

--- src/components/pricing/pricing-faq.tsx
+++ src/components/pricing/pricing-faq.tsx
@@ -11,9 +11,13 @@ export function PricingFaq() {
     <Accordion type="single" collapsible className="w-full">
       {faqConfig.items.map((faqItem) => (
         <AccordionItem key={faqItem.id} value={faqItem.id}>
-          <AccordionTrigger className="text-base">{faqItem.question}</AccordionTrigger>
+          <AccordionTrigger className="text-base">
+            <div className="text-left w-full">{faqItem.question}</div>
+          </AccordionTrigger>
           <AccordionContent className="text-base text-muted-foreground whitespace-pre-wrap">
-            {faqItem.answer}
+            {/* {faqItem.answer} */}
+            {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
+            <div dangerouslySetInnerHTML={{ __html: faqItem.answer }} />
           </AccordionContent>
         </AccordionItem>
       ))}

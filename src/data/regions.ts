ee7f021749e5e9402780a39d1a24333312cbab1d
Corey <coreyralston@gmail.com>
2025-10-03 23:03:08 -0700

    Fix import: use regions list from src/data/regions to avoid circular UI import

---
+++ src/data/regions.ts
@@ -0,0 +1,24 @@
+export const regionsList = [
+  "Los Angeles County",
+  "Orange County",
+  "San Diego County",
+  "San Francisco Bay Area",
+  "Sacramento Area",
+  "Central Valley",
+  "New York City",
+  "Long Island",
+  "Westchester County",
+  "Atlanta Metro",
+  "Miami-Dade County",
+  "Broward County",
+  "Chicago Metro",
+  "Dallas-Fort Worth",
+  "Houston Metro",
+  "Phoenix Metro",
+  "Denver Metro",
+  "Seattle Metro",
+  "Portland Metro",
+  "Online/Virtual",
+];
+
+

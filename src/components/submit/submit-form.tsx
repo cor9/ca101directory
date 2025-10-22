2021932a8848eac7747cb61cbcb94763ef56cbef
Corey <35475851+cor9@users.noreply.github.com>
2025-10-22 04:54:31 -0700

    Replace 'Basic' option with 'Standard' in select

--- src/components/submit/submit-form.tsx
+++ src/components/submit/submit-form.tsx
@@ -410,9 +410,8 @@ export function SubmitForm({ tagList, categoryList }: SubmitFormProps) {
                           className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-paper focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                         >
                           <option value="Free">Free</option>
-                          <option value="Basic">Basic</option>
+                          <option value="Standard">Standard</option>
                           <option value="Pro">Pro</option>
-                          <option value="Premium">Premium</option>
                         </select>
                       </FormControl>
                       <FormMessage />

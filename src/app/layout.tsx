5422e64aa7cc662e805d3bceebc35c9b90699d5f
Corey <coreyralston@gmail.com>
2025-11-15 04:42:12 -0800

    fix(meta/script): use meta content attr; add Impact STAT script in head via next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ import { constructMetadata } from "@/lib/metadata";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 export const metadata = constructMetadata();
 
@@ -38,7 +39,10 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="impact-site-verification" value="72d79363-1a4b-42ec-be63-d24c04b73026" />
+        <meta name="impact-site-verification" content="72d79363-1a4b-42ec-be63-d24c04b73026" />
+        <Script id="impact-stat" strategy="afterInteractive">
+          {`(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A4798761-60ea-47a3-9269-a8e8a25503651.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`}
+        </Script>
       </head>
       <body
         className={cn(
